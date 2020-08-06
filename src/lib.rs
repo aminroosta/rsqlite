3123c74a8c14a1c989b42201ba18e35612933a00
amin roosta <amin.roosta@outlook.com>
2020-08-06 18:14:06 +0800

    add collect

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,35 @@
 //! # Ergonamic sqlite library with zero overhead.
 //!
 //! ```
-//! use rsqlite::{Database, Result};
+//! # use rsqlite::*;
+//! 
+//! let database = Database::open(":memory:")?;
 //!
-//! fn foo() -> Result<()> {
-//!     let database = Database::open(":memory:")?;
+//! // execute a query with no arguments
+//! database.execute(r#"
+//!    create table if not exists user (
+//!       id integer primary key autoincrement not null,
+//!       age int,
+//!       name text,
+//!       weight real
+//!    );"#, ())?;
 //!
-//!     // execute a query with no arguments
-//!     database.execute(r#"
-//!        create table if not exists user (
-//!           id integer primary key autoincrement not null,
-//!           age int,
-//!           name text,
-//!           weight real
-//!        );"#, ())?;
+//! // execute with parameters
+//! database.execute(
+//!    "insert into user(age, name, weight) values(?, ?, ?)",
+//!    (29, "amin", 69.5)
+//! )?;
 //!
-//!     // execute with parameters
-//!     database.execute(
-//!        "insert into user(age, name, weight) values(?, ?, ?)",
-//!        (29, "amin", 69.5)
-//!     )?;
+//! // collect example
+//! let age: i32 = database.collect("select age from user limit 1", ())?;
+//! assert!(age == 29);
 //!
-//!     Ok(())
-//! }
+//! let info: (i32, String, f64) = database.collect(
+//!     "select age, name, weight from user limit 1", ()
+//! )?;
+//! assert!(info == (29, "amin".to_owned(), 69.5));
+//!
+//! # Ok::<(), RsqliteError>(())
 //! ```
 #![allow(incomplete_features)]
 #![feature(specialization)]
@@ -45,9 +52,18 @@ pub struct Statement {
     pub stmt: *mut ffi::sqlite3_stmt,
 }
 
+/// Bindable types can bind themselves to a sqlite statement
 pub trait Bindable {
-    /// binds &self to the sqlite statement and returns the next `index`
+    /// given an index, binds itself and returns the next index
     fn bind(&self, statement: &Statement, index: c_int) -> Result<c_int>;
+}
+/// Collectable types can be parsed from the columns of the sqlite result row
+pub trait Collectable
+where
+    Self: Sized,
+{
+    /// given an column, collects itself and returns the next column
+    fn collect(statement: &Statement, column: &mut c_int) -> Self;
 }
 
 impl Database {
@@ -119,7 +135,7 @@ impl Database {
     /// if you need to return data, you should use `.query()`
     pub fn execute(&self, sql: &str, arg_or_args: impl Bindable) -> Result<()> {
         let statement = self.prepare(sql)?;
-        arg_or_args.bind(&statement, 0)?;
+        arg_or_args.bind(&statement, 1)?;
 
         let retcode = unsafe { ffi::sqlite3_step(statement.stmt) };
 
@@ -127,6 +143,25 @@ impl Database {
             ffi::SQLITE_DONE => Ok(()),
             other => Err(other.into()),
         }
+    }
+
+    pub fn collect<R>(&self, sql: &str, arg_or_args: impl Bindable) -> Result<R>
+    where
+        R: Collectable,
+    {
+        let statement = self.prepare(sql)?;
+        arg_or_args.bind(&statement, 1)?;
+
+        let retcode = unsafe { ffi::sqlite3_step(statement.stmt) };
+
+        let result = match retcode {
+            ffi::SQLITE_ROW => Ok(R::collect(&statement, &mut 0)),
+            other => Err(other.into()),
+        };
+
+        let _ = unsafe { ffi::sqlite3_reset(statement.stmt) };
+
+        result
     }
 }
 
@@ -147,6 +182,16 @@ impl Drop for Statement {
             ffi::sqlite3_finalize(self.stmt);
             self.stmt = ptr::null_mut();
         }
+    }
+}
+
+/// allow binding with `&T` if `T` is Bindable
+impl<T> Bindable for &T
+where
+    T: Bindable,
+{
+    fn bind(&self, statement: &Statement, index: c_int) -> Result<c_int> {
+        (*self).bind(statement, index)
     }
 }
 
@@ -173,6 +218,7 @@ impl Bindable for c_double {
         }
     }
 }
+/// sqlite3_bind_text() expects a pointer to well-formed UTF8 text (i.e `&str`)
 impl<'a> Bindable for &'a str {
     fn bind(&self, statement: &Statement, index: c_int) -> Result<c_int> {
         let len = self.as_bytes().len() as c_int;
@@ -205,3 +251,51 @@ where
         Ok(index)
     }
 }
+
+impl Collectable for () {
+    fn collect(_statement: &Statement, _column: &mut c_int) -> Self {}
+}
+impl Collectable for c_int {
+    fn collect(statement: &Statement, column: &mut c_int) -> Self {
+        let result = unsafe { ffi::sqlite3_column_int(statement.stmt, *column) };
+        *column += 1;
+        result
+    }
+}
+impl Collectable for c_double {
+    fn collect(statement: &Statement, column: &mut c_int) -> Self {
+        let result = unsafe { ffi::sqlite3_column_double(statement.stmt, *column) };
+        *column += 1;
+        result
+    }
+}
+impl Collectable for String {
+    fn collect(statement: &Statement, column: &mut c_int) -> Self {
+        let ptr = unsafe { ffi::sqlite3_column_text(statement.stmt, *column) };
+        let bytes = unsafe { ffi::sqlite3_column_bytes(statement.stmt, *column) };
+
+        *column += 1;
+
+        match bytes == 0 {
+            true => String::new(),
+            false => unsafe {
+                let slice = std::slice::from_raw_parts(ptr as *const u8, bytes as usize);
+                String::from_utf8_unchecked(slice.to_owned())
+            },
+        }
+    }
+}
+impl<T0, T1, T2> Collectable for (T0, T1, T2)
+    where T0 : Collectable,
+          T1: Collectable,
+          T2: Collectable,
+{
+    fn collect(statement: &Statement, column: &mut c_int) -> Self {
+        let t0 = T0::collect(statement, column);
+        let t1 = T1::collect(statement, column);
+        let t2 = T2::collect(statement, column);
+
+        (t0, t1, t2)
+    }
+}
+
